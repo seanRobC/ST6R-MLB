ff45a4275608233b6a9af9412c7fd3fe89db4862
Dale Tyler <edt@tylerent.com>
2019-05-06 14:54:53 -0700

    Updates for doxygen in datastore

--- mlb/datastore/MLBTeam.hpp
+++ mlb/datastore/MLBTeam.hpp
@@ -17,25 +17,74 @@ using namespace std;
 struct EdgeGreater;
 struct EdgeGreaterRev;
 
-// used to describe the distance (edges) between Teams/Parks
+//! TeamEdge Class - used to describe the distance (edges) between Teams/Parks
+//!
+//! \author edt (5/6/19)
 class TeamEdge
 {
 public:
     int m_nTeam;
     int m_nDistance;
     bool m_bVisited;
+    //! TeamEdge - default constructor
+    //!
+    //! \author edt (5/6/19)
     TeamEdge()
       : m_nTeam(0), m_nDistance(0), m_bVisited(false) {};
+
+    //! TeamEdge - constuctor used to load distances from the CSV file
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param teamx - team number
+    //! \param distancex - dictance to that team's stadium
     TeamEdge(int teamx, int distancex)
       : m_nTeam(teamx), m_nDistance(distancex), m_bVisited(false) {};
+
+    //! TeamEdge Copy constructor
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param src - item to be duplicated
     TeamEdge(const TeamEdge &src)
     : m_nTeam(src.m_nTeam), m_nDistance(src.m_nDistance), m_bVisited(src.m_bVisited) {};
+
+    //! TeamEdge - operator =
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param rhs - Item being duplicated
+    //!
+    //! \return TeamEdge&amp; 
     TeamEdge &operator=(const TeamEdge &rhs);
+
+    //! ClearVisited - marks this edge not visited for DFS and BFS traversals
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
     void ClearVisited(void) { m_bVisited = false; };
+
+    //! SetVisited - marks this edge visted for DFS/BFS traversals
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
     void SetVisited(void) { m_bVisited = true; };
+
+    //! IsVisited - determines if this edge visited
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return bool - true if visited, false otherwise
     bool IsVisited(void) { return(m_bVisited); };
 };
 
+//! EdgeGreater - function object used to sort TeamEdges by ascending distance
+//!
+//! \author edt (5/6/19)
 struct EdgeGreater
 {
     bool operator()(const TeamEdge &s1, const TeamEdge &s2) const
@@ -44,6 +93,9 @@ struct EdgeGreater
     }
 };
 
+//! EdgeGreaterRev - function object used to sort TeamEdges by descending distance
+//!
+//! \author edt (5/6/19)
 struct EdgeGreaterRev
 {
     bool operator()(const TeamEdge &s1, const TeamEdge &s2) const
@@ -52,12 +104,26 @@ struct EdgeGreaterRev
     }
 };
 
+//! MLB_Souvenir class - stores number, name, price of Souvenirs
+//!
+//! \author edt (5/6/19)
 class MLB_Souvenir
 {
 public:
+    //! MLB_Souvenir default constructor
+    //!
+    //! \author edt (5/6/19)
     MLB_Souvenir()
       :  m_bInitialized(false) {};
  
+    //! MLB_Souvenir - used to load Souvenirs from csv data store
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param number 
+    //! \param name 
+    //! \param price 
+    //! \param deleted 
     MLB_Souvenir(const int number, const string &name,
                  const float price, const bool deleted)
         : m_nNumber(number), m_bDeleted(deleted),
@@ -66,11 +132,23 @@ public:
 
     ~MLB_Souvenir() {};
 
+    //! MLB_Souvenir copy constructor
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param src - item being duplicated
     MLB_Souvenir( MLB_Souvenir const &src)
       : m_nNumber(src.m_nNumber), m_bDeleted(src.m_bDeleted),
         m_sSouvenirName(src.m_sSouvenirName), m_fSouvenirPrice(src.m_fSouvenirPrice),
         m_bInitialized(src.m_bInitialized) {};
 
+    //! MLB_Souvenir - operator =
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param rhs - item being duplicated
+    //!
+    //! \return MLB_Souvenir&amp; 
     MLB_Souvenir& operator=(const MLB_Souvenir& rhs);
 
     int     m_nNumber;
@@ -80,6 +158,9 @@ public:
     bool    m_bInitialized;
 };
 
+//! SouvenirbyName - used to sort by name (ascending) for the priority_queue
+//!
+//! \author edt (5/6/19)
 struct SouvenirbyName
 {
     bool operator()(const MLB_Souvenir &s1, const MLB_Souvenir &s2)
@@ -88,6 +169,9 @@ struct SouvenirbyName
     }
 };
 
+//! SouvenirbyNameRev- used to sort by name (descending) for the priority_queue
+//!
+//! \author edt (5/6/19)
 struct SouvenirbyNameRev
 {
     bool operator()(const MLB_Souvenir &s1, const MLB_Souvenir &s2)
@@ -96,11 +180,16 @@ struct SouvenirbyNameRev
     }
 };
 
+//! MLBTeam - contains information about a team and its Stadium, available souvenirs, and distances to other stadiums
+//!
+//! \author edt (5/6/19)
 class MLBTeam
 {
 friend class TeamDataStore;
 public:
-    // Constructor
+    //! MLBTeam default constructor
+    //!
+    //! \author edt (5/6/19)
     MLBTeam()
      : m_nNumber(0), m_bInitialized(false) { std::vector<TeamEdge> x; m_Distances = x; };
 
@@ -114,56 +203,316 @@ public:
     MLBTeam& operator=(const MLBTeam& rhs);
 
     // Getters
+
+    //! getNumber - returns team number
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return int - team number
     int           getNumber(void)   const { return m_nNumber; };
+
+    //! getTeamName - returns team name
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp; - team name
     const string &getTeamName(void) const { return m_sTeamName; };
+
+    //! getStadiumName - returns stadium name
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp; - stadium name
     const string &getStadiumName(void) const{return m_sParkName;};
+
+    //! getCapacity - returns stadium capacity
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return int - stadium capacity
     int           getCapacity(void) const { return m_nSeatingCapacity; };
+
+    //! getLocation - returns city and state of stadium
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp; - city and state of stadium
     const string &getLocation(void) const {return m_sLocation;};
+
+    //! getSurface - returns stadium surface type
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp; - stadium surface type
     const string &getSurface(void) const {return m_sPlayingSurface;};
+
+    //! getLeague - returns MLB (National, American) the team plays in
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp; - MLB (National, American) the team plays in
     const string &getLeague(void) const {return m_sLeague;};
+
+    //! getDateOpened - returns year park opened
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return int - year park opened
     int           getDateOpened(void) const { return m_nYearOpened; };
+
+    //! getCenterField - returns distance in feet to center field wall
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return int - distance in feet to center field wall
     int           getCenterField(void) const { return m_nDistCenterFieldFeet; };
     int           getDistCenterFieldFeet (void) const { return m_nDistCenterFieldFeet; };
     int           getDistCenterFieldMeters(void) const { return m_nDistCenterFieldMeters; };
+
+    //! getTypology - returns type of stadium
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp; -type of stadium 
     const string &getTypology(void) const {return m_sParkTyplolgy;};
+
+    //! getRoofType - returns type of roof on stadium
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return const string&amp;  - type of roof on stadium
     const string &getRoofType(void) const {return m_sRoofType;};
 
     const vector<TeamEdge> &GetDistances(void) const { return m_Distances; };
 
     // Setters
+
+    //! updateTeamName sets new team name
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param name - new team name
     void updateTeamName(const string &name) { m_sTeamName = name; };
+
+    //! updateStadiumName - sets new stadium name
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param park - new stadium name
     void updateStadiumName(const string &park) { m_sParkName = park; };
+
+    //! updateCapacity - sets new stadium capacity
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param capacity - new stadium capacity
     void updateCapacity(int capacity) { m_nSeatingCapacity = capacity; };
+
+    //! updateLocation - sets new stadium location city/state
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param location - new stadium location city/state
     void updateLocation(const string &location) { m_sLocation = location; };
+
+    //! updateSurface - sets new stadium surface
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param surface - new stadium surface
     void updateSurface(const string &surface) { m_sPlayingSurface = surface; };
+
+    //! updateLeague - sets new MLB designation
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param league - new MLB designation
     void updateLeague(const string &league) { m_sLeague = league; };
+
+    //! updateDateOpened - sets new date opened
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param opened - new date opened
     void updateDateOpened(int opened) { m_nYearOpened = opened; };
+
+    //! updateCenterField - set new center field distance in feet
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param centerfield new center field distance in feet
     void updateCenterField(int centerfield) { m_nDistCenterFieldFeet = centerfield; m_nDistCenterFieldMeters = (int)(centerfield * 0.330f); };
     void updateDistCenterFieldFeet (int centerfieldft) { m_nDistCenterFieldFeet = centerfieldft; };
     void updateDistCenterFieldMeters(int centerfieldmtrs) { m_nDistCenterFieldMeters =centerfieldmtrs; };
+
+    //! updateTypology - set new stadium typeology
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param typology - new stadium typeology
     void updateTypology(const string &typology) { m_sParkTyplolgy = typology; };
+
+    //! updateRoofType - set new stadium roof type
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param roof - new stadium roof type
     void updateRoofType(const string &roof) { m_sRoofType = roof; };
-    // not used - void updateSouvenirs(const vector<MLB_Souvenir> &souvenirs) { m_Souvenirs = souvenirs; };
 
     // Helper functions for maintenance
+    //! IsDeleted - query if team deleted and should not display in UI
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return bool - true if team deleted and should not display in UI
     bool IsDeleted(void) const {return m_bDeleted;};
+
+    //! SetDeleted - set team deleted status
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param deleted - team deleted status
+    //!
+    //! \return bool - team deleted status
     bool SetDeleted(bool deleted) {return (m_bDeleted = deleted);}
+
+    //! PrintAsDebug - prints state of team object
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param print_endl 
+    //! \param print_detail 
+    //!
+    //! \return bool - true if object printed
     bool PrintAsDebug(bool print_endl, bool print_detail) const;
 
     // Souvenir Accessors
+    //!
+   
+    //! GetSouvenirs - returns a vector of Souvenirs
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param souvenirs - vector of Souvenirs
     void GetSouvenirs(vector<MLB_Souvenir> &souvenirs) const;
+
+    //! addSouvenir - add a new Souvenir
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param desc - description
+    //! \param price - price
     void addSouvenir(const string desc, float price);
+
+    //! deleteSouvenir - deleets a Souvenir
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param number - souvenir number
+    //!
+    //! \return bool - true if deleted
     bool deleteSouvenir(int number);
+
+    //! updateSouvenir - updates an existing Souvenir
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param number - number used to locate item ti be updates
+    //! \param desc - new description
+    //! \param price - new price
+    //!
+    //! \return bool - true if update succeeded 
     bool updateSouvenir(int number, const string desc, float price);
 
     // DFS and BFS helpers
+
+    //! ClearVisited - clear DFS/BFS visited flag
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
     void ClearVisited(void) { m_bVisited = false; };
+
+    //! SetVisited - sets DFS/BFS visited flag
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
     void SetVisited(void) { m_bVisited = true; };
+
+    //! IsVisited - query if visited during traversal
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
+    //!
+    //! \return bool - true if visited
     bool IsVisited(void) { return(m_bVisited); };
+
+    //! ClearVisited - clears visited flag on a specific edge
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param edge - edge number to clear
     void ClearVisited(int edge) { m_Distances[edge].m_bVisited = false; };
+
+
+    //! SetVisited - sets visited flag on a specific edge
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param edge - edge number to set
     void SetVisited(int edge) { m_Distances[edge].m_bVisited = true; };
+
+    //! IsEdgeVisited - query if this edge visited during traversal
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param edge - number to query
+    //!
+    //! \return bool - true if visited
     bool IsEdgeVisited(int edge) { return(m_Distances[edge].m_bVisited); };
+
+    //! GetDistance - get the distance for this edge
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param edge - number to query
+    //!
+    //! \return int - edge distance
     int GetDistance(int edge) { return(m_Distances[edge].m_nDistance); };
+
+    //! GetAdjacentCity - get adjacent team number for this edge
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param edge - edge number to query
+    //!
+    //! \return int - adjacent team number 
     int GetAdjacentCity(int edge) { return(m_Distances[edge].m_nTeam); };
         
 
@@ -203,6 +552,9 @@ private:
 };
 
 
+//! Cmp_by_teamnumber - function object used to sort MLBTeams by ascending number
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_teamnumber {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -211,6 +563,9 @@ struct Cmp_by_teamnumber {
     }
 };
 
+//! Cmp_by_teamname - function object used to sort MLBTeams by ascending team name
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_teamname {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -219,6 +574,9 @@ struct Cmp_by_teamname {
     }
 };
 
+//! Cmp_by_stadiumname - function object used to sort MLBTeams by ascending stadium name
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_stadiumname {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -227,6 +585,9 @@ struct Cmp_by_stadiumname {
     }
 };
 
+//! Cmp_by_seatcapacity - function object used to sort MLBTeams by ascending number of seats
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_seatcapacity {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -235,6 +596,9 @@ struct Cmp_by_seatcapacity {
     } 
 };
 
+//! Cmp_by_seatcapacityrev - function object used to sort MLBTeams by decending number of seats
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_seatcapacityrev {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -243,6 +607,9 @@ struct Cmp_by_seatcapacityrev {
     } 
 };
 
+//! Cmp_by_typology - function object used to sort MLBTeams by ascending park typology
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_typology {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -251,6 +618,9 @@ struct Cmp_by_typology {
     } 
 };
 
+//! Cmp_by_yearopen - function object used to sort MLBTeams by ascending year park opened
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_yearopen {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
@@ -259,6 +629,9 @@ struct Cmp_by_yearopen {
     } 
 };
 
+//! Cmp_by_distcenterfield - function object used to sort MLBTeams by ascending distance to center field
+//!
+//! \author edt (5/6/19)
 struct Cmp_by_distcenterfield {
     bool operator()(MLBTeam &s1, MLBTeam &s2) const
     {
--- mlb/datastore/MLBTeamDataStore.hpp
+++ mlb/datastore/MLBTeamDataStore.hpp
@@ -28,46 +28,174 @@ typedef std::vector<TeamEdge> adjacency_list_t;
 class TeamDataStore
 {
 public:
-    // Constructor
+
+    //! TeamDataStore - default constructor
+    //!
+    //! \author edt (5/6/19)
     TeamDataStore();
 
     // Destructor
     virtual ~TeamDataStore();
 
+
+    //! PrintAsDebug - prints state of teams list object
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param print_endl 
+    //! \param print_detail 
+    //!
+    //! \return bool - true if object printed
     void printAsDebug(bool printeol, bool printcontent) const;
+
+    //! FindbyNumber - Return a MLBTeam object based on supplied number
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param Number - team number to locate
+    //!
+    //! \return MLBTeam&amp; - reference to MLBTeam object
     MLBTeam &FindbyNumber(int Number);
+
     std::vector<MLBTeam>::const_iterator getBeginIterator(void) {return m_TeamList.cbegin();};
     std::vector<MLBTeam>::const_iterator getEndIterator(void) {return m_TeamList.cend();};
 
+    //! load - load the m_TeamList member from user specified csv file
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param path - path to csv file
+    //! \param ItemsAreAdditional - true if append csv items to m_TeamList
     void load(const string path, bool ItemsAreAdditional = false);
+
+    //! save - store the contents from m_TeamList into specified csv file
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param path - location to store csv file (.tmp suffix)
     void save(const string path);
+
+    //! load_additional - helper to allow appending to m_TeamList
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param path 
     void load_additional(const string path);
 
     // DFS and BFS Helpers
     // Note extra row/column to allow 1 based vertice numbers
     static TeamEdge m_DistancesArray[31+1][31+1]; // Adjacency Array
     static std::vector<std::vector<TeamEdge>> m_DistancesArrayV; // Adjacency Array
+
+    //! InitVisited - clear all MLBTeam adjacency lists visited flag
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
     void InitVisited(void);
+
+    //! InitVisitedArray - clear adjacency array, then load distances from all MLBTeam objects
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param void 
     void InitVisitedArray(void);
 
-
+    //! DijkstraComputePaths - determine shortest distance to all teams
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param source - starting team number
+    //! \param teamlist - list of all MLBTeams
+    //! \param adjacency_list - priority_queue used to sort distances
+    //! \param min_distance - vecor of distances to all cities
+    //! \param previous - vector of all parents used in search
+    //! \param num_vertices - number of teams
     void DijkstraComputePaths(int source,const std::vector<MLBTeam> teamlist,
                               const adjacency_list_t &adjacency_list,
                               std::vector<int> &min_distance,
                               std::vector<int> &previous, int num_vertices);
 
+    //! DijkstraGetShortestPathTo - get list of stadium names used to get to specified team
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param vertex - team number to search for
+    //! \param teamlist - list of all MLBTeams
+    //! \param previous - list of parent nodes
+    //!
+    //! \return std::list&lt;string&gt; - vector of team names in path order
     std::list<string> DijkstraGetShortestPathTo(int vertex, const std::vector<MLBTeam> teamlist, const std::vector<int> &previous);
+
+    //! PlanTrip - get shortest distance from source to destination team 
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param from - starting team
+    //! \param to - ending team
+    //!
+    //! \return int - shortest distance to destination
     int PlanTrip(int from, int to);
+
+    //! PlanShortestTrip - shortest distance from startingteam through specified list of teams
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param from - starting team
+    //! \param dests- vector of teams to visit
+    //! \param ordered - true if teams should be visited in order specified - not shortest distance
+    //!
+    //! \return const std::vector&lt;TeamEdge&gt; - vector if teams to visit in order
     const std::vector<TeamEdge> PlanShortestTrip(int from, std::vector<int> dests, bool ordered);
+
+
+    //! PlanMultTrip - visit all teams starting at specified team
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param source_team - location to start
+    //!
+    //! \return const std::vector&lt;TeamEdge&gt; 
     const std::vector<TeamEdge> PlanMultTrip(int source_team);
+
+    //! primMST - determine Minimum Spanning Tree (MST) from specifed starting team
+    //!           using Prim Algorithm
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param source_team - starting team number
     void primMST(int source_team);
+
+    //! DFS - preform a depth first traversal starting at specified location, 
+    //!       calculate total length 
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param vertice  - starting team
+    //! \param total_miles - pointer to location to store total distance
     void DFS(const int vertice, int &total_miles);
+
+
+    //! BFS - preform a breadth first traversal starting at specified location, 
+    //!       calculate total length 
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param vertice  - starting team
+    //! \param total_miles - pointer to location to store total distance
     void BFS(const int vertice, int &total_miles);
 
     std::vector<MLBTeam> m_TeamList;
     static const int max_weight;
 
 private:
+    //! DuplicateNumPresent - verify team number not already in m_TeamList
+    //!
+    //! \author edt (5/6/19)
+    //!
+    //! \param Number - team number to search for
+    //!
+    //! \return bool - true if duplicate team number
     bool DuplicateNumPresent(int Number);
 
     // Copy constructor
